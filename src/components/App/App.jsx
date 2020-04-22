988837a932c4f90aed89bd748d801b9348df4303
Alex Dyksa <odyksa@gmail.com>
2020-04-22 14:55:53 +0300

    deleted excess files

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,38 +1,13 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import styles from './App.scss';
-import ErrorBoundary from '../ErrorBoundary';
-import ScrollToTop from '../../components/ScrollToTop';
-import PageNotFound from '../../components/PageNotFound';
 
 class App extends Component {
-   render() {
-      return (
-         <ErrorBoundary>
-            <Router basename="/">
-               <ScrollToTop>
-                  <div className="app">
-                     <Switch>
-                        <Route
-                           path="/"
-                           exact
-                           component={() => <div>HomePage</div>}
-                        />
-                        <Route
-                           path="/about"
-                           exact
-                           component={() => <div>About</div>}
-                        />
-                        <Route
-                           component={PageNotFound}
-                        />
-                     </Switch>
-                  </div>
-               </ScrollToTop>
-            </Router>
-         </ErrorBoundary>
-      );
-   }
+  render() {
+    return (
+      <div className="app">App</div>
+    );
+  }
 }
 
 export default App;
