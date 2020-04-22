128e6750e7eaf330b372f25d63e26a633c08461e
Alex Dyksa <odyksa@gmail.com>
2020-04-22 15:27:22 +0300

    add store

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
 
 import './index.scss';
 
 import App from './components/App';
+import { store } from './store';
 
 
-render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
